51155c8b370f763ed65406b8b4c276871c8728c1
dragounv <110033160+dragounv@users.noreply.github.com>
2024-02-02 15:00:23 +0100

    Add variables for java opts

--- central-dir/start-crawler.sh
+++ central-dir/start-crawler.sh
@@ -9,6 +9,7 @@ javao_Xmx=${3}
 javao_Xms=${4}
 
 export HERITRIX_HOME=${5}
-export JAVA_HOME=${6}  
-export JAVA_OPTS="-server -Xmx12000M -Xms256M -XX:+UseParallelGC -Dcom.sun.management.jmxremote.port=5000 -Djava.rmi.server.hostname=${heritrix_bind} -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
+export JAVA_HOME=${6}
+export JAVA_OPTS="-server -Xmx$javao_Xmx -Xms$javao_Xms -XX:+UseParallelGC"  
+#export JAVA_OPTS="-server -Xmx12000M -Xms256M -XX:+UseParallelGC -Dcom.sun.management.jmxremote.port=5000 -Djava.rmi.server.hostname=${heritrix_bind} -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
 $HERITRIX_HOME/bin/heritrix -a $heritrix_user:$heritrix_password -b $heritrix_bind -p $heritrix_port
--- project-dir/settings-continuous.cfg.sh
+++ project-dir/settings-continuous.cfg.sh
@@ -5,12 +5,12 @@ ADDRESS='<ip-address>'
 PORT='<port>' # 7778
 HERITRIX_HOME='<heritrix_home>' #/opt/heritrix/running
 JAVA_HOME='<java_home>'			#/opt/java/testing
-JAVAO_XMX='12000'
-JAVAO_XMS='256'
+JAVAO_XMX='4G' # set like "4G" or "4000M"
+JAVAO_XMS='256M'
 CRAWLER_HOST='<crawler_host_name>'
 CRAWLER_JOBS='<crawler_jobs_path>'
 CRAWLER_CENT_DIR='<crawler_central_opt_path>' #/opt/heritrix
-CRAWLER_CENT_DIR='/Users/zdenkovozar/GIT/continuous-suite/central-dir'
+#CRAWLER_CENT_DIR='/Users/zdenkovozar/GIT/continuous-suite/central-dir'
 
 #Crawl Metadata - Dates
 TODAY_DATE=$(date +'%Y-%m-%d')
@@ -20,13 +20,13 @@ YEAR=$(date +'%y')
 TYP='<Continuous-ProjectName>'    #Key project name
 TYP_LC=$(echo "$TYP" | tr '[:upper:]' '[:lower:]')
 TYPPATH="${CRAWLER_JOBS}/Crawler-config/${TYP}"
-TYPPATH='/Users/zdenkovozar/GIT/continuous-suite/project-dir'
+#TYPPATH='/Users/zdenkovozar/GIT/continuous-suite/project-dir'
 NAME_SHORT="<SpecialName>${YEAR}"
 M_COMMENT='<Commentary about crawl topic>'
 
 #Crawl Metadata - Paths
 CORE_STORE="<mnt_store_path/>${YEAR}"
-CORE_STORE="<mnt_store_path/>${YEAR}" #alternative path commented in template
+#CORE_STORE="<mnt_store_path/>${YEAR}" #alternative path commented in template
 
 # Crawl Metadata - Organizational
 
